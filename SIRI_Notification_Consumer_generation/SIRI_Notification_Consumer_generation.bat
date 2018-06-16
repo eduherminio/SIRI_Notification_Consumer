3ddc3978c96775a63010140bb41722372e3b45e0
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-06-16 19:48:25 +0200

    Rename SIRI_Consumer_generation script

--- SIRI_Notification_Consumer_generation/SIRI_Notification_Consumer_generation.bat
+++ SIRI_Notification_Consumer_generation/SIRI_Notification_Consumer_generation.bat
@@ -1,5 +1,7 @@
 SET gSOAP_DIR=C:\external\gSOAP\
 
+echo This script requires gSOAP library to be installed in C:\external\gSOAP
+
 %gSOAP_DIR%\gsoap\bin\win32\wsdl2h -t%gSOAP_DIR%\gsoap\typemap.dat -oSIRI.h ..\SIRI-2.0o-xsd\siri_wsConsumer-Document.wsdl 
 PAUSE
 %gSOAP_DIR%\gsoap\bin\win32\soapcpp2 -c++11 -j -I%gSOAP_DIR%\gsoap\import -I%gSOAP_DIR%\gsoap SIRI.h
